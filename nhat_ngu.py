cd5588059e0ac5fbe9c541a4a4b8f0af19b9cbe8
Tran Viet Hoa <22010142>
2022-11-29 09:14:25 +0700

    New file

---
+++ nhat_ngu.py
@@ -0,0 +1,7 @@
+def welcom (name):
+        print("Chào Mừng đại ca {}".format(name))
+def chia (n1,n2):
+    if n2==0 :
+        print ("Không cha dc cho số 0")
+    else:
+        print ("Kết quả là {:.3f}".format(n1/n2))
